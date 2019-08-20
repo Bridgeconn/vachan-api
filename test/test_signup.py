c2b70fe75067e9f8a845f80497a647279546e195
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-08-20 17:08:53 +0530

    Update test_signup.py

--- test/test_signup.py
+++ test/test_signup.py
@@ -7,8 +7,8 @@ def sign_up(url, FirstName, LastName,EmailAddress,Password):
 	data = {'Firstname':FirstName,
             'Lastname':LastName,
             'Email':EmailAddress,
-			'Password':Password}
-	resp = requests.post(url, data=data)
+		'Password':Password}
+	resp = requests.post(url,data=data)
 	return resp
 
 def test_signup_load():
@@ -17,8 +17,8 @@ def test_signup_load():
 	assert resp.status_code == 200, resp.text
 
 @pytest.mark.parametrize("FirstName,LastName,EmailAddress, Password",[('ag','ag','ag17@yopmail.com',"1189")])
-def test_sugnup_user(supply_url,FirstName,LastName,EmailAddress,Password):
-	resp = sign_up(supply_url,FirstName,LastName,EmailAddress,Password)
+def test_sugnup_user(url,FirstName,LastName,EmailAddress,Password):
+	resp = sign_up(url,FirstName,LastName,EmailAddress,Password)
 	j = json.loads(resp.text)
 	assert resp.status_code == 201, resp.text
 	assert "accessToken" in j, "success="+str(j['success'])+j['message']
