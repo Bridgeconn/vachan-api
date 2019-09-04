55d04f80045a9ce26ccffeaaf9fd4c519965f98c
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-04 11:40:41 +0530

    Update test_listorgs.py

--- test/test_listorgs.py
+++ test/test_listorgs.py
@@ -8,7 +8,7 @@ def supply_url():
 
 
 @pytest.fixture
-def get_accessToken():
+def get_adm_accessToken():
 	email = "alex@yopmail.com"
 	password = "1189"
 	url = "https://stagingapi.autographamt.com/v1/auth"
@@ -24,6 +24,19 @@ def get_accessToken():
 def get_supAdmin_accessToken():
 	email = 'savitha.mark@bridgeconn.com'
 	password = '221189'
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+			'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+	return token
+
+@pytest.fixture
+def get_trans_accessToken():
+	email = 'ag2@yopmail.com'
+	password = '1189'
 	url = "https://stagingapi.autographamt.com/v1/auth"
 	data = {'email':email,
 			'password':password}
@@ -87,9 +100,9 @@ def test_getlistorg(supply_url,get_supAdmin_accessToken):
 	assert 'userId' in j[0], j[0]
 	
 # 	## GET method with access token
-def test_getlistorgw(supply_url,get_accessToken):
+def test_getlistorgwa(supply_url,get_adm_accessToken):
 	url = supply_url + '/v1/autographamt/organisations'
-	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_accessToken)})
+	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
 	j = json.loads(resp.text)
 	print(j)
 	assert resp.status_code == 200, resp.text
@@ -101,4 +114,18 @@ def test_getlistorgw(supply_url,get_accessToken):
 	assert 'organisationEmail' in j[0], j[0] 
 	assert 'verified' in j[0], j[0]
 	assert 'userId' in j[0], j[0]
-	
+
+def test_getlistorgwt(supply_url,get_trans_accessToken):
+	url = supply_url + '/v1/autographamt/organisations'
+	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
+	j = json.loads(resp.text)
+	print(j)
+	assert resp.status_code == 200, resp.text
+	assert isinstance (j,list), j
+	assert 'organisationId' in j[0], j[0]
+	assert 'organisationName' in j[0], j[0]
+	assert 'organisationAddress' in j[0], j[0]
+	assert 'organisationPhone' in j[0], j[0]
+	assert 'organisationEmail' in j[0], j[0] 
+	assert 'verified' in j[0], j[0]
+	assert 'userId' in j[0], j[0]
