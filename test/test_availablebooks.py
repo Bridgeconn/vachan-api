eed279470cb322d6605d34ec38eb2e03435ca0b7
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-11 12:13:17 +0530

    Update test_availablebooks.py

--- test/test_availablebooks.py
+++ test/test_availablebooks.py
@@ -58,44 +58,6 @@ def test_firstpage_load():
 	resp = requests.get(url)
 	assert resp.status_code == 200, resp.text
 
-@pytest.mark.parametrize("email, password",[("alex@yopmail.com",'1189')])
-def test_list_valid_user(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert "accessToken" in j, "success="+str(j['success'])+j['message']
-
-@pytest.mark.parametrize("email, password",[("ag2@yopmail.com",'1189')])
-def test_list_valid_userk(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert "accessToken" in j, "success="+str(j['success'])+j['message']
-
-@pytest.mark.parametrize("email, password",[('ag27@yopmail.com',"letme")])
-def test_list_invalid_password(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, "success="+str(j['success'])
-	assert j['message'] == "Incorrect Password", 'message='+j['message']
-
-@pytest.mark.parametrize("email, password",[('kavitharaju@gmail.com',"letme"),('kavitharaju@bridgeconn.com',"letme")])
-def test_list_invalid_email(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, "success="+str(j['success'])
-	assert j['message'] == "This email is not registered", 'message='+j['message']
-
-@pytest.mark.parametrize("email, password",[('kavitha.raju@bridgeconn.com',"letmepass")])
-def test_list_notverified_email(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, "success="+str(j['success'])
-	assert j['message'] == "Email is not Verified", 'message='+j['message']
-
 ## GET method with access token for list users
 def test_aviailbooksup(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/sources/books/30'
