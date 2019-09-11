7896f81bfea5d38feb11b0d73d15559dbea8c177
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-11 12:24:58 +0530

    Update test_listorgs.py

--- test/test_listorgs.py
+++ test/test_listorgs.py
@@ -58,32 +58,7 @@ def test_firstpage_load():
 	resp = requests.get(url)
 	assert resp.status_code == 200, resp.text
 
-@pytest.mark.parametrize("email, password",[("savitha.mark@bridgeconn.com",'221189')])
-def test_list_valid_user(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert "accessToken" in j, "success="+str(j['success'])+j['message']
-
-
-@pytest.mark.parametrize("email, password",[('savitha.mark@bridgeconn.com',"letme")])
-def test_list_invalid_password(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, "success="+str(j['success'])
-	assert j['message'] == "Incorrect Password", 'message='+j['message']
-
-@pytest.mark.parametrize("email, password",[('savithaa.mark@bridgeconn.com',"221189")])
-def test_list_invalid_email(supply_url,email,password):
-	resp = check_login(supply_url,email,password)
-	j = json.loads(resp.text)
-	print(j)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, "success="+str(j['success'])
-	assert j['message'] == "This email is not registered", 'message='+j['message']
-
-# GET method with access token
+# GET method for organization
 def test_getlistorg(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/autographamt/organisations'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
