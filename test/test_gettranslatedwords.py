d1a5678417a490f92f5ac8d242eb61a245402f2a
Savithamark <savithamark.093@gmail.com>
2019-09-24 14:15:37 +0530

    Tst cases for translatedwords

---
+++ test/test_gettranslatedwords.py
@@ -0,0 +1,81 @@
+  # -- coding: utf - 8 --
+import pytest
+import requests
+import json
+
+@pytest.fixture
+def supply_url():
+	return "https://stagingapi.autographamt.com"
+
+
+@pytest.fixture
+def get_adm_accessToken():
+	email = "alex@yopmail.com"
+	password = "1189"
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+			'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+	return token
+
+@pytest.fixture
+def get_supAdmin_accessToken():
+	email = 'savitha.mark@bridgeconn.com'
+	password = '221189'
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+			'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+	return token
+
+@pytest.fixture
+def get_trans_accessToken():
+	email = 'ag2@yopmail.com'
+	password = '1189'
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+			'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+
+def test_getTranslatedwordssup(supply_url,get_supAdmin_accessToken):
+	url = supply_url + '/v1/translations/30/18/करो'
+	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200, resp.text
+	assert j['success'] == False, str(j)
+	assert j['message'] == "No Translation or sense available for this token", str(j)
+
+def test_getTranslatedwordsad(supply_url,get_adm_accessToken):
+	url = supply_url + '/v1/translations/30/18/करो'
+	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200, resp.text
+	# assert isinstance(j,list), j
+	assert j['success'] == False, str(j)
+	assert j['message'] == "No Translation or sense available for this token", str(j)
+	
+def test_getTranslatedwordstr(supply_url,get_trans_accessToken):
+	url = supply_url + '/v1/translations/30/18/करो'
+	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200, resp.text
+	# assert isinstance(j,list), j
+	# print (j)
+
+def test_getTranslatedwordstr2(supply_url,get_trans_accessToken):
+	url = supply_url + '/v1/translations/30/18/कपडे'
+	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200, resp.text
+	assert j['success'] == False, str(j)
+	assert j['message'] == "No Translation or sense available for this token", str(j)
+
