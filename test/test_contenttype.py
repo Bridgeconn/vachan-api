50732e5295546fa213eabf47ca9a6b620ee9c258
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-24 11:42:54 +0530

    Update test_contenttype.py

--- test/test_contenttype.py
+++ test/test_contenttype.py
@@ -39,57 +39,61 @@ def get_trans_accessToken():
 	password = '1189'
 	url = "https://stagingapi.autographamt.com/v1/auth"
 	data = {'email':email,
-			'password':password}
+		'password':password}
 	resp = requests.post(url, data=data)
 	respobj = json.loads(resp.text)
 	token = respobj['accessToken']
+	return token
+	
+# @pytest.fixture
+# def get_trans_accessToken3():
+# 	email = 'kavitharaju18@gmail.com'
+# 	password = '111111'
+# 	url = "https://stagingapi.autographamt.com/v1/auth"
+# 	data = {'email':email,
+# 			'password':password}
+# 	resp = requests.post(url, data=data)
+# 	respobj = json.loads(resp.text)
+# 	token = respobj['accessToken']
+# 	return token
 
-
-
-def check_login(url,email,password):
-	url = url + "/v1/auth" 
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	return resp
-
-def test_firstpage_load():
-	url = "https://staging.autographamt.com"
-	resp = requests.get(url)
-	assert resp.status_code == 200, resp.text
-
-## GET method with access token for list users
-def test_getUserProjectssup(supply_url,get_supAdmin_accessToken):
+def test_Contenttypesup(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/contenttypes'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
 	assert 'contentType' in j[0], j[0]
 	assert 'contentId' in j[0], j[0]
  
 	
 
-def test_getUserProjectad(supply_url,get_adm_accessToken):
+def test_Contenttypead(supply_url,get_adm_accessToken):
 	url = supply_url + '/v1/contenttypes'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
 	assert 'contentType' in j[0], j[0]
 	assert 'contentId' in j[0], j[0]
    
 
 
-def test_getUserProjecttr(supply_url,get_trans_accessToken):
+def test_Contenttypetr(supply_url,get_trans_accessToken):
 	url = supply_url + '/v1/contenttypes'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
 	assert 'contentType' in j[0], j[0]
 	assert 'contentId' in j[0], j[0]
+	 
+# def test_Contenttypetr2(supply_url,get_trans_accessToken3):
+# 	url = supply_url + '/v1/contenttypes'
+# 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken3)})
+# 	j = json.loads(resp.text)
+# 	assert resp.status_code == 200, resp.text
+# 	assert j['success'] == False, str(j)
+# 	assert j['message'] == "There are no contents available", str(j)
+  
   
