c6bc5b33b2ac4ea2605e1e36bfa51443ffd60840
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-24 11:53:41 +0530

    Update test_getalllanguages.py

--- test/test_getalllanguages.py
+++ test/test_getalllanguages.py
@@ -44,31 +44,14 @@ def get_trans_accessToken():
 	respobj = json.loads(resp.text)
 	token = respobj['accessToken']
 
-
-
-def check_login(url,email,password):
-	url = url + "/v1/auth" 
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	return resp
-
-def test_firstpage_load():
-	url = "https://staging.autographamt.com"
-	resp = requests.get(url)
-	assert resp.status_code == 200, resp.text
-
-
-## GET method with access token for list users
 def test_GetAllaLnguagessup(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/languages'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
-	# assert 'languageName' in j[0], j[0]
-	# assert 'languageId' in j[0], j[0]
+	assert 'languageName' in j[0], j[0]
+	assert 'languageId' in j[0], j[0]
     # assert 'languageCode' in j[0], j[0]
 	
 
@@ -78,9 +61,8 @@ def test_GetAllaLnguagesad(supply_url,get_adm_accessToken):
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
-	# assert 'languageName' in j[0], j[0]
-	# assert 'languageId' in j[0], j[0]
+	assert 'languageName' in j[0], j[0]
+	assert 'languageId' in j[0], j[0]
     # assert 'languageCode' in j[0], j[0]
 	
 
@@ -91,9 +73,8 @@ def test_GetAllaLnguagestr(supply_url,get_trans_accessToken):
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
-	# assert 'languageName' in j[0], j[0]
-	# assert 'languageId' in j[0], j[0]
+	assert 'languageName' in j[0], j[0]
+	assert 'languageId' in j[0], j[0]
     # assert 'languageCode' in j[0], j[0]
 	
   
