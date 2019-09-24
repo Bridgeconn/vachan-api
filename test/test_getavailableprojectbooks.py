98a227aaa858a4c44847eb10b2407de41431a719
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-24 12:01:11 +0530

    Update test_getavailableprojectbooks.py

--- test/test_getavailableprojectbooks.py
+++ test/test_getavailableprojectbooks.py
@@ -33,7 +33,17 @@ def get_accessTokentr():
 	token = respobj['accessToken']
 
 	return token	
-
+# @pytest.fixture
+# def get_trans_accessToken2():
+# 	email = 'kavitharaju18@gmail.com'
+# 	password = '111111'
+# 	url = "https://stagingapi.autographamt.com/v1/auth"
+# 	data = {'email':email,
+# 			'password':password}
+# 	resp = requests.post(url, data=data)
+# 	respobj = json.loads(resp.text)
+# 	token = respobj['accessToken']
+# 	return token
 
 @pytest.fixture
 def get_supAdmin_accessToken():
@@ -48,36 +58,30 @@ def get_supAdmin_accessToken():
 
 	return token
 
-
-def check_login(url,email,password):
-	url = url + "/v1/auth" 
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	return resp
-
-def test_firstpage_load():
-	url = "https://staging.autographamt.com"
-	resp = requests.get(url)
-	assert resp.status_code == 200, resp.text
-
 def test_getavailableprojbookad(supply_url,get_accessTokenadm):
 	url = supply_url + '/v1/sources/projects/books/35/11'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_accessTokenadm)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
-	print (j)
+	
 	
 def test_getavailableprojbooksad(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/sources/projects/books/35/11'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
-	print (j)
+	
 
 def test_getavailableprojbookstr(supply_url,get_accessTokentr):
 	url = supply_url + '/v1/sources/projects/books/35/11'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_accessTokentr)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
-	print(j)
+
+# def test_availableprojectbookstr2(supply_url,get_trans_accessToken2):
+# 	url = supply_url + '/v1/sources/books/31'
+# 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken2)})
+# 	j = json.loads(resp.text)
+# 	assert resp.status_code == 200, resp.text
+# 	assert j['success'] == False, str(j)
+# 	assert j['message'] == "No data available", str(j)
