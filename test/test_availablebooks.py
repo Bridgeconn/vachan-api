48ee32069b326f9d6551e365f1722f4bc9aad0f3
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-24 10:36:11 +0530

    Update test_availablebooks.py

--- test/test_availablebooks.py
+++ test/test_availablebooks.py
@@ -46,42 +46,33 @@ def get_trans_accessToken():
 
 
 
-def check_login(url,email,password):
-	url = url + "/v1/auth" 
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	return resp
+#def check_login(url,email,password):
+#	url = url + "/v1/auth" 
+#	data = {'email':email,
+#			'password':password}
+#	resp = requests.post(url, data=data)
+#	return resp
 
-def test_firstpage_load():
-	url = "https://staging.autographamt.com"
-	resp = requests.get(url)
-	assert resp.status_code == 200, resp.text
-
-## GET method with access token for list users
 def test_aviailbooksup(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/sources/books/30'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
-	assert isinstance(j,list), j
-	print (j)
+	#print (j)
 
 def test_aviailbookad(supply_url,get_adm_accessToken):
 	url = supply_url + '/v1/sources/books/30'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
-	assert isinstance(j,list), j
-	print (j)
+	#print (j)
 
 def test_aviailbooktr(supply_url,get_trans_accessToken):
 	url = supply_url + '/v1/sources/books/30'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
-	assert isinstance(j,list), j
-	print (j)
+	#print (j)
 	
 	
   
