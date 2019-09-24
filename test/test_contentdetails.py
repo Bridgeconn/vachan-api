03d570a8d9656452af3258cb42ad652a2f70ce20
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-09-24 11:33:11 +0530

    Update test_contentdetails.py

--- test/test_contentdetails.py
+++ test/test_contentdetails.py
@@ -44,52 +44,35 @@ def get_trans_accessToken():
 	respobj = json.loads(resp.text)
 	token = respobj['accessToken']
 
-
-
-def check_login(url,email,password):
-	url = url + "/v1/auth" 
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	return resp
-
-def test_firstpage_load():
-	url = "https://staging.autographamt.com"
-	resp = requests.get(url)
-	assert resp.status_code == 200, resp.text
-
-## GET method with access token for list users
-def test_getUserProjectssup(supply_url,get_supAdmin_accessToken):
+def test_Contentdetailssup(supply_url,get_supAdmin_accessToken):
 	url = supply_url + '/v1/contentdetails'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
 	assert 'contentType' in j[0], j[0]
 	assert 'contentId' in j[0], j[0]
  
 	
 
-def test_getUserProjectad(supply_url,get_adm_accessToken):
+def test_Contentdetailsad(supply_url,get_adm_accessToken):
 	url = supply_url + '/v1/contentdetails'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
 	assert 'contentType' in j[0], j[0]
 	assert 'contentId' in j[0], j[0]
    
 
 
-def test_getUserProjecttr(supply_url,get_trans_accessToken):
+def test_Contentdetailstr(supply_url,get_trans_accessToken):
 	url = supply_url + '/v1/contentdetails'
 	resp = requests.get(url,headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert isinstance(j,list), j
-	print (j)
+	
 	assert 'contentType' in j[0], j[0]
 	assert 'contentId' in j[0], j[0]
   
