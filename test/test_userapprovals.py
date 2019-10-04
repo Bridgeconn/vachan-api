1345a0ad1386511eacd6682871f222426ae0fc82
Savithamark <51701168+Savithamark@users.noreply.github.com>
2019-10-04 13:30:38 +0530

    Update test_userapprovals.py

--- test/test_userapprovals.py
+++ test/test_userapprovals.py
@@ -46,51 +46,12 @@ def get_trans_accessToken():
 
 	return token
 
-@pytest.fixture
-def get_supAdmin_accessToken():
-	email = 'savitha.mark@bridgeconn.com'
-	password = '221189'
-	url = "https://stagingapi.autographamt.com/v1/auth"
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	respobj = json.loads(resp.text)
-	token = respobj['accessToken']
 
-	return token
-
-@pytest.mark.parametrize('userId,admin',[('11','admin')])
-def test_Userapprovalsup(supply_url,get_supAdmin_accessToken,userId,admin):
+@pytest.mark.parametrize('userId,admin',[('12','admin')])
+def test_Userapprovalsup2(supply_url,get_trans_accessToken,userId,admin):
 	url = supply_url + '/v1/autographamt/approvals/users'
 	data = {
-			'userId':11,
-			'admin':admin
-			}
-	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, str(j)
-	assert j['message'] == "Unauthorized", str(j)
-
-@pytest.mark.parametrize('userId,admin',[('11','admin')])
-def test_Userapprovalad(supply_url,get_adm_accessToken,userId,admin):
-	url = supply_url + '/v1/autographamt/approvals/users'
-	data = {
-			'userId':11,
-			'admin':admin
-			}
-	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
-	j = json.loads(resp.text)
-	assert resp.status_code == 200, resp.text
-	assert j['success'] == False, str(j)
-	assert j['message'] == "Unauthorized", str(j)
-
-
-@pytest.mark.parametrize('userId,admin',[('11','admin')])
-def test_Userapprovalad(supply_url,get_trans_accessToken,userId,admin):
-	url = supply_url + '/v1/autographamt/approvals/users'
-	data = {
-			'userId':11,
+			'userId':userId,
 			'admin':admin
 			}
 	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
@@ -99,18 +60,28 @@ def test_Userapprovalad(supply_url,get_trans_accessToken,userId,admin):
 	assert j['success'] == False, str(j)
 	assert j['message'] == "Unauthorized", str(j)
 
-
+@pytest.mark.parametrize('userId,admin',[('11','admin')])
+def test_Userapprovaltr(supply_url,get_trans_accessToken,userId,admin):
+	url = supply_url + '/v1/autographamt/approvals/users'
+	data = {
+			'userId':userId,
+			'admin':admin
+			}
+	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200, resp.text
+	assert j['success'] == False, str(j)
+	assert j['message'] == "Unauthorized", str(j)
     
 @pytest.mark.parametrize('userId,admin',[('11','admin')])
 def test_Userapprovalsupc(supply_url,get_supAdmin_accessToken,userId,admin):
 	url = supply_url + '/v1/autographamt/approvals/users'
 	data = {
-			'userId':11,
+			'userId':userId,
 			'admin':admin
 			}
 	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
 	j = json.loads(resp.text)
-	print(j)
 	assert resp.status_code == 200, resp.text
 	assert j['success'] == True, str(j)
 	assert j['message'] == "Role Updated", str(j)
@@ -119,7 +90,7 @@ def test_Userapprovalsupc(supply_url,get_supAdmin_accessToken,userId,admin):
 def test_Userapprovaladc(supply_url,get_adm_accessToken,userId,admin):
 	url = supply_url + '/v1/autographamt/approvals/users'
 	data = {
-			'userId':11,
+			'userId':userId,
 			'admin':admin
 			}
 	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
