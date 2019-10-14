2b6d1a7df89702a6c3aef2532367245a41ccaf2a
kavitharaju <kavitharaju18@gmail.com>
2019-10-14 13:19:34 +0530

    Add two more testcases in createproject
    - for unautorized attempt, by a translator
    - for duplicate project

--- test/test_createproject.py
+++ test/test_createproject.py
@@ -36,8 +36,8 @@ def get_supAdmin_accessToken():
 
 @pytest.fixture
 def get_accessTokentrans():
-	email = 'kavitharaju18@gmail.com'
-	password = '111111'
+	email = 'ag2@yopmail.com'
+	password = '1189'
 	url = "https://stagingapi.autographamt.com/v1/auth"
 	data = {'email':email,
 			'password':password}
@@ -56,10 +56,9 @@ def test_createprojecttr(supply_url,get_accessTokentrans,sourceId,targetLanguage
 		}
 	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_accessTokentrans)})
 	j = json.loads(resp.text)
-	print(j)
 	assert resp.status_code == 200, resp.text
-	assert j['success'] == True, str(j)
-	assert j['message'] == "Project created", str(j)
+	assert j['success'] == False, str(j)
+	assert j['message'] == "UnAuthorized", str(j)
 	### add code to delete the project
 
 @pytest.mark.parametrize('sourceId,targetLanguageID,organisationId',[(56,4923,26)])
@@ -71,7 +70,7 @@ def test_createprojectsup1(supply_url,get_supAdmin_accessToken,sourceId,targetLa
     	'organisationId':organisationId
 		}
 	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
-	j = json.dumps(resp.text)
+	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert j['success'] == True, str(j)
 	assert j['message'] == "Project created", str(j)
@@ -90,4 +89,19 @@ def test_createprojectad(supply_url,get_supAdmin_accessToken,sourceId,targetLang
 	assert resp.status_code == 200, resp.text
 	assert j['success'] == True, str(j)
 	assert j['message'] == "Project created", str(j)
+
+
+@pytest.mark.parametrize('sourceId,targetLanguageID,organisationId',[(56,3699,13)])
+def test_createproject_existing(supply_url,get_supAdmin_accessToken,sourceId,targetLanguageID,organisationId):
+	url = supply_url + '/v1/autographamt/organisations/projects'
+	data = {
+			'sourceId':sourceId,
+    		'targetLanguageId':targetLanguageID,
+    		'organisationId':organisationId
+			}
+	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200, resp.text
+	assert j['success'] == False, str(j)
+	assert j['message'] == "Project already created", str(j)
 	### add code to delete the project
