1147466556fee83198be4bfa25a65e8c6d11df01
kavitharaju <kavitharaju18@gmail.com>
2019-10-14 13:48:34 +0530

    fix createproject assignment testcases
    - remove auth token fixtures and auth header
    - remove role based test cases
    - change the types of parameters from string to int and array of strings
    - add testcases/assertions based on whether its a new translator or existing translator

--- test/test_createprojectassignment.py
+++ test/test_createprojectassignment.py
@@ -6,83 +6,31 @@ import json
 def supply_url():
 	return "https://stagingapi.autographamt.com"
 
-# @pytest.fixture
-# def get_accessTokenadm():
-# 	email = "alex@yopmail.com"
-# 	password = "1189"
-# 	url = "https://stagingapi.autographamt.com/v1/auth"
-# 	data = {'email':email,
-# 			'password':password}
-# 	resp = requests.post(url, data=data)
-# 	respobj = json.loads(resp.text)
-# 	token = respobj['accessToken']
 
-# 	return token
-
-@pytest.fixture
-def get_accessTokentr():
-	email = "ag2@yopmail.com"
-	password = "1189"
-	url = "https://stagingapi.autographamt.com/v1/auth"
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	respobj = json.loads(resp.text)
-	token = respobj['accessToken']
-
-	return token
-
-@pytest.fixture
-def get_supAdmin_accessToken():
-	email = 'savitha.mark@bridgeconn.com'
-	password = '221189'
-	url = "https://stagingapi.autographamt.com/v1/auth"
-	data = {'email':email,
-			'password':password}
-	resp = requests.post(url, data=data)
-	respobj = json.loads(resp.text)
-	token = respobj['accessToken']
-
-	return token
-
-
-# @pytest.mark.parametrize('userID,projectID,Books',[("14","35",'act')])
-# def test_projectassignmentAd(supply_url,get_accessTokenadm,userID, projectID,Books):
-# 	url = supply_url + '/v1/autographamt/projects/assignments'
-# 	data = {
-# 			'userId': userID,
-# 			'projectId': projectID,
-# 			'Books': Books
-# 			}
-# 	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_accessTokenadm)})
-# 	j = json.loads(resp.text)
-# 	print(j)
-# 	assert resp.status_code == 200, resp.text
-# 	assert j['success'] == True, str(j)
-# 	assert j['message'] == "User Role Updated", str(j)
-
-@pytest.mark.parametrize('userID,projectID,Books',[("30","37",'rev')])
-def test_projectassignmentsup(supply_url,get_supAdmin_accessToken,userID, projectID,Books):
+@pytest.mark.parametrize('userID,projectID,Books',[(30,52,["rev"])])
+def test_projectassignment_newtanslator(supply_url,userID, projectID,Books):
 	url = supply_url + '/v1/autographamt/projects/assignments'
 	data = {'userId': userID,
 			'projectId': projectID,
 			'books': Books
 			}
-	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
+	resp = requests.post(url,data=json.dumps(data))
 	j = json.loads(resp.text)
+	print(j)
 	assert resp.status_code == 200, resp.text
 	assert j['success'] == True, str(j)
-	assert j['message'] == "User Role Updated", str(j)
+	assert j['message'] == "User Role Assigned", str(j)
+	### add code to delete assignment
 
 
-@pytest.mark.parametrize('userID,projectID,Books',[("30","37",'rev')])
-def test_projectassignmenttr(supply_url,get_accessTokentr,userID, projectID,Books):
+@pytest.mark.parametrize('userID,projectID,Books',[(30,52,["mat"])])
+def test_projectassignment_existingTranslator(supply_url,userID, projectID,Books):
 	url = supply_url + '/v1/autographamt/projects/assignments'
 	data = {'userId': userID,
 			'projectId': projectID,
 			'books': Books
 			}
-	resp = requests.post(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_accessTokentr)})
+	resp = requests.post(url,data=json.dumps(data))
 	j = json.loads(resp.text)
 	assert resp.status_code == 200, resp.text
 	assert j['success'] == True, str(j)
