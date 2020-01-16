9d99604a173050336877ded2cf6310167db744c0
Savithamark <savithamark.093@gmail.com>
2020-01-16 10:34:05 +0530

    Test case for activate and delete API's

---
+++ test/test_deleteproject.py
@@ -0,0 +1,94 @@
+import pytest
+import requests
+import json
+
+@pytest.fixture
+def supply_url():
+	return "https://stagingapi.autographamt.com"
+
+
+@pytest.fixture
+def get_adm_accessToken():
+	email = "alex@yopmail.com"
+	password = "1189"
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+		'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+	return token
+
+@pytest.fixture
+def get_supAdmin_accessToken():
+	email = 'savitha.mark@bridgeconn.com'
+	password = '221189'
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+		'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+	return token
+
+@pytest.fixture
+def get_trans_accessToken():
+	email = 'ag2@yopmail.com'
+	password = '1189'
+	url = "https://stagingapi.autographamt.com/v1/auth"
+	data = {'email':email,
+		'password':password}
+	resp = requests.post(url, data=data)
+	respobj = json.loads(resp.text)
+	token = respobj['accessToken']
+
+	return token
+
+@pytest.mark.parametrize('projectId',[("35")])
+def test_delete_Project_1(supply_url,get_supAdmin_accessToken,projectId):
+	url = supply_url + "/v1/autographamt/project/delete"
+	data = {
+            'projectId': projectId
+	}
+	resp = requests.delete(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200
+	assert j['message'] == "Deactivated project."
+
+
+@pytest.mark.parametrize('projectId',[("35")])
+def test_delete_Project_2(supply_url,get_supAdmin_accessToken,projectId):
+	url = supply_url + "/v1/autographamt/project/delete"
+	data = {
+            'projectId': projectId
+	}
+	resp = requests.delete(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_supAdmin_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200
+	assert j['message'] == "Deactivated project."
+
+
+@pytest.mark.parametrize('projectId',[("35")])
+def test_delete_Project_3(supply_url,get_trans_accessToken,projectId):
+	url = supply_url + "/v1/autographamt/project/delete"
+	data = {
+            'projectId': projectId
+	}
+	resp = requests.delete(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_trans_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200
+	assert j['message'] == "UnAuthorized! Only the organisation admin or super admin can delete projects."
+
+
+@pytest.mark.parametrize('projectId',[("35")])
+def test_delete_Project_4(supply_url,get_adm_accessToken,projectId):
+	url = supply_url + "/v1/autographamt/project/delete"
+	data = {
+            'projectId': projectId
+	}
+	resp = requests.delete(url,data=json.dumps(data),headers={'Authorization': 'bearer {}'.format(get_adm_accessToken)})
+	j = json.loads(resp.text)
+	assert resp.status_code == 200
+	assert j['message'] == "Deactivated project."
