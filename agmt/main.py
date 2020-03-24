aeaeb363b5f4ff875f15d566eae7e7b0c1f3185e
RevantCI <revantcherian@gmail.com>
2020-03-24 11:00:31 +0530

    #50: fetch video metadata filter by language option

--- agmt/main.py
+++ agmt/main.py
@@ -2876,6 +2876,8 @@ def getInfographics(languageCode):
     except Exception as ex:
         traceback.print_exc()
         return '{"success":false, "message":"%s"}' % (str(ex))
+
+
 def sortAudioBibles(languageObject,audioBible):
     '''Sort the list of audio bible's by language format by language object.'''
     for index,item in enumerate(languageObject):
@@ -2887,6 +2889,7 @@ def sortAudioBibles(languageObject,audioBible):
         language = audioBible.pop("language")
         languageObject.append({"language": language,"audioBibles": [audioBible]})
     return languageObject
+
 @app.route("/v1/audiobibles", methods=["GET"])
 def getAudioBibles():
     '''Fetch the metadata for the audiobibles Option to filter by language.'''
@@ -2916,6 +2919,75 @@ def getAudioBibles():
         # Group and sort dictionaries by book
         audio_bibles = reduce(sortAudioBibles, audio_bibles, [])
         return json.dumps(audio_bibles)
+    except Exception as ex:
+        traceback.print_exc()
+        return '{"success":false, "message":"%s"}' % (str(ex))
+		
+def sortVideosByLanguage(languageObject,video):
+    '''Sort the list of video's by language object.'''
+    for index,item in enumerate(languageObject):
+        if item["language"]["name"] == video["language"]["name"]:
+            video.pop("language")
+            languageObject[index]["books"].append(video)
+            break
+    else:
+        language = video.pop("language")
+        languageObject.append({"language": language,"books": [video]})
+    return languageObject
+
+def sortVideosByBooks(languageObject):
+    '''Sort the list of video's by book code.'''
+    bookObject = {}
+    for item in languageObject["books"]:
+        book = item.pop("book")
+        if book in bookObject.keys():
+            bookObject[book].append(item)
+        else:
+            bookObject[book]= [item]
+    languageObject["books"] = bookObject
+    return languageObject
+
+@app.route("/v1/videos", methods=["GET"])
+def getVideos():
+    '''Fetch the metadata for the videos with an option to filter by language.'''
+    try:
+        connection = get_db()
+        cursor = connection.cursor()
+        query = "select books,url,title,description,theme,v.language_id,language_name,language_code \
+            from bible_videos v inner join languages l on v.language_id=l.language_id"
+        #use language code param to filter by language
+        lang_code = request.args.get('language')
+        if lang_code and lang_code.strip():
+            cursor.execute("select language_id from languages where language_code=%s", (lang_code,))
+            language_id = cursor.fetchone()
+            if not language_id or language_id is None:
+                return '{"success": false, "message":""message":"language code not available.""}'
+            cursor.execute(query + " and l.language_id in(%s)", (language_id[0],))
+        else:
+            cursor.execute(query)
+        rst = cursor.fetchall()
+        if not rst:
+            return '{"success":false, "message":"No videos available"}'
+        videos = []
+        ot_books = "gen,exo,lev,num,deu,jos,jdg,rut,1sa,2sa,1ki,2ki,1ch,2ch,ezr,neh,est,job,psa,\
+            pro,ecc,sng,isa,jer,lam,ezk,dan,hos,jol,amo,oba,jon,mic,nam,hab,zep,hag,zec,mal"
+        nt_books ="mat,mrk,luk,jhn,act,rom,1co,2co,gal,eph,php,col,1th,2th,1ti,2ti,tit,phm,heb,\
+            jas,1pe,2pe,1jn,2jn,3jn,jud,rev"
+        for book, url, title, description, theme, language_id, name, code in rst:
+            if book == "OT":
+                book = ot_books
+            elif book == "NT":
+                book = nt_books
+            elif book == "FB":
+                book = ot_books+nt_books
+            books = book.split(",")
+            for book_code in books:
+            	videos.append({ 'book':book_code.strip(), 'title':title, 'url':url, 'description':description,
+                 'theme':theme, 'language':{'name':name,'code':code,'id':language_id}})
+        # Group and sort dictionaries by book
+        videos = reduce(sortVideosByLanguage, videos, [])
+        videos = map(sortVideosByBooks, videos)
+        return json.dumps(list(videos))
     except Exception as ex:
         traceback.print_exc()
         return '{"success":false, "message":"%s"}' % (str(ex))
