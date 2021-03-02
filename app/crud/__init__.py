240362c96b6bba2f61ef5d8e381beee6eabec4b7
kavitharaju <kavitharaju18@gmail.com>
2021-03-02 11:37:41 +0530

    unicode normalize all text contents before writing to DB

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,1 +1,7 @@
 '''An empty file to show this folder is a python module'''
+import unicodedata
+
+def normalize_unicode(text):
+    '''to normalize text contents before adding them to DB'''
+    return unicodedata.normalize('NFC', text)
+    
