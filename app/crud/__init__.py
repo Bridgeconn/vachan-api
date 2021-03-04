b7c2ee130d3bc6b9b56c4ee2b03e7434a00efea8
kavitharaju <kavitharaju18@gmail.com>
2021-03-04 11:48:01 +0530

    change unicode normalization method to NFKC

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,7 +1,7 @@
-'''An empty file to show this folder is a python module'''
+'''A file to show this folder is a python module and define shared functions and objects'''
 import unicodedata
 
 def normalize_unicode(text):
     '''to normalize text contents before adding them to DB'''
-    return unicodedata.normalize('NFC', text)
+    return unicodedata.normalize('NFKC', text)
     
