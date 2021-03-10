8520c12218f722f963ab5c2bdf5dcd913b9a8424
kavitharaju <kavitharaju18@gmail.com>
2021-03-10 17:01:46 +0530

    add table creations required for AgMT

--- db/seed_DB.sql
+++ db/seed_DB.sql
@@ -80,3 +80,43 @@ CREATE TABLE public.bible_books_look_up (
 );
 
 \COPY bible_books_look_up (book_id,book_name, book_code) FROM 'bible_books.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE public.translation_projects(
+    project_id SERIAL PRIMARY KEY,
+    project_name TEXT unique NOT NULL,
+    source_lang_id int NOT NULL
+        REFERENCES languages(language_id),
+    target_lang_id int NOT NULL 
+        REFERENCES languages(language_id),
+    source_document_format text DEFAULT 'Bible USFM',
+    active boolean default true,
+    metadata jsonb,
+    created_user int NULL,
+    created_at timestamp with time zone DEFAULT NOW()
+    last_updated_user int NULL,
+    last_updated_at  timestamp with time zone DEFAULT NOW(),
+);
+
+CREATE TABLE public.translation_drafts(
+    draft_id SERIAL PRIMARY KEY,
+    project_id int NOT NULL 
+        REFERENCES translation_projects(project_id) ON DELETE CASCADE,
+    sentence_id int NOT NULL,
+    sentence text,
+    draft text,
+    draft_meta jsonb[],
+    last_updated_user int NULL,
+    last_updated_at  timestamp with time zone DEFAULT NOW(),
+    UNIQUE(project_id, sentence_id)
+);
+
+CREATE TABLE public.translation_memory(
+    token_id int PRIMARY KEY,
+    source_lang_id int NOT NULL
+        REFERENCES languages(language_id),
+    target_lang_id int NOT NULL 
+        REFERENCES languages(language_id),
+    token text NOT NULL,
+    translations text[],
+    UNIQUE(source_lang_id, target_lang_id, token)
+);
