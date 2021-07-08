a3fef8400a18b206216563c10d40fba8184919b2
siju <siju.moncy@gmail.com>
2021-07-08 13:29:06 +0530

    linting done

--- app/graphql_api/mutations.py
+++ app/graphql_api/mutations.py
@@ -2,37 +2,37 @@
 
 
 import graphene
-import json
-from graphql import language
-from sqlalchemy.sql import schema
 
 #pylint: disable=E0401
 #pylint gives import error if relative import is not used. But app(uvicorn) doesn't accept it
 
-from crud import structurals_crud, contents_crud, projects_crud, nlp_crud
+from crud import structurals_crud
 from dependencies import get_db
 from graphql_api import types, utils
-from graphql_api.types import Metadata
 import schemas
 
 
 ############ ADD NEW Language #################
-""" ADD Language Input """
 class InputAddLang(graphene.InputObjectType):
+    """ADD Language Input"""
     language = graphene.String()
     code = graphene.String(description="language code as per bcp47(usually 2 letter code)")
     scriptDirection = graphene.String()
     metaData = graphene.JSONString()
 
-
+#pylint: disable=R0901,too-few-public-methods
 class AddLanguage(graphene.Mutation):
+    """Mutation class for Add Language"""
     class Arguments:
-        language_Addargs = InputAddLang(required=True)
+        """Arguments declaration for the mutation"""
+        language_addargs = InputAddLang(required=True)
 
+#pylint: disable=R0201,no-self-use
     language = graphene.Field(types.Language)
-    '''resolve'''
-    def mutate(root,info,language_Addargs):
-        schema_model = utils.convert_graphene_obj_to_pydantic(language_Addargs,schemas.LanguageCreate)
+    def mutate(self,info,language_addargs):
+        '''resolve'''
+        schema_model = utils.convert_graphene_obj_to_pydantic\
+            (language_addargs,schemas.LanguageCreate)
         result =structurals_crud.create_language(db_=next(get_db()),lang=schema_model)
         language = types.Language(
             language = result.language,
@@ -44,8 +44,8 @@ class AddLanguage(graphene.Mutation):
 
 
 ####### Update Language ##############
-""" update Language Input """
 class InputUpdateLang(graphene.InputObjectType):
+    """ update Language Input """
     languageId = graphene.Int()
     language = graphene.String()
     code = graphene.String(description="language code as per bcp47(usually 2 letter code)")
@@ -53,16 +53,19 @@ class InputUpdateLang(graphene.InputObjectType):
     metaData = graphene.JSONString()
 
 class UpdateLanguage(graphene.Mutation):
+    """ Mutation for update language"""
     class Arguments:
+        """ Argumnets declare for mutations"""
         language_updateargs = InputUpdateLang(required=True)
 
     language = graphene.Field(types.Language)
 
-    def mutate(root,info,language_updateargs):
-        schema_model = utils.convert_graphene_obj_to_pydantic(language_updateargs,schemas.LanguageEdit)
-        result = structurals_crud.update_language(db_=next(get_db()),lang=schema_model)  
-        print("==============================================")
-        print(result)
+#pylint: disable=R0201,no-self-use
+    def mutate(self,info,language_updateargs):
+        """resolver"""
+        schema_model = utils.convert_graphene_obj_to_pydantic\
+            (language_updateargs,schemas.LanguageEdit)
+        result = structurals_crud.update_language(db_=next(get_db()),lang=schema_model)
         language = types.Language(
             language = result.language,
             code = result.code,
@@ -77,4 +80,3 @@ class VachanMutations(graphene.ObjectType):
     '''All defined mutations'''
     add_language = AddLanguage.Field()
     update_language = UpdateLanguage.Field()
-
