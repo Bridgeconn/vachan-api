f05386f152564af15c21bfe4ba668bb4ac90d279
kavitharaju <kavitharaju18@gmail.com>
2021-11-05 20:44:35 +0530

    use posgres port from env

--- app/database.py
+++ app/database.py
@@ -9,9 +9,10 @@ postgres_host = os.environ.get("VACHAN_POSTGRES_HOST", "localhost")
 postgres_user = os.environ.get("VACHAN_POSTGRES_USER", "postgres")
 postgres_database = os.environ.get("VACHAN_POSTGRES_DATABASE", "vachan")
 postgres_password = os.environ.get("VACHAN_POSTGRES_PASSWORD", "secret")
+postgres_port = os.environ.get("VACHAN_POSTGRES_PORT", "5432")
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://%s:%s@%s/%s"%(
-	postgres_user,postgres_password,postgres_host,postgres_database)
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://%s:%s@%s:%s/%s"%(
+	postgres_user,postgres_password,postgres_host, postgres_port,postgres_database)
 
 connection_url = os.environ.get("VACHAN_POSTGRES_CONN", SQLALCHEMY_DATABASE_URL)
 
