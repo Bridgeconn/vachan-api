108542d6e47d5436adb3a177cdb890861b1503b9
anjaly <anjalyv75@gmail.com>
2021-11-15 13:20:50 +0530

    Removed stop word functions stored earlier. Keeping in new file

--- app/crud/nlp_crud.py
+++ app/crud/nlp_crud.py
@@ -956,33 +956,3 @@ def obtain_agmt_source(db_:Session, project_id, books=None, sentence_id_range=No
         obj = {"sentenceId": row.sentenceId, "surrogateId":row.surrogateId,"sentence":row.sentence}
         result.append(obj)
     return result
-
-###################### Stop words identification ######################
-def retrieve_stopwords(db_: Session, language_code, **kwargs):
-    '''retreives stopwords of a given language from look up table'''
-    include_system_defined = kwargs.get("include_system_defined", True)
-    include_user_defined = kwargs.get("include_user_defined", True)
-    include_auto_generated = kwargs.get("include_auto_generated", True)
-    only_active = kwargs.get("only_active", True)
-    skip = kwargs.get("skip", 0)
-    limit = kwargs.get("limit", 100)
-    query = db_.query(db_models.Language.languageId)
-    language_id = query.filter(func.lower(db_models.Language.code) == language_code.lower()).first()
-    if not language_id:
-        raise NotAvailableException("Language with code %s, not in database"%language_code)
-    query = db_.query(db_models.StopWords)
-    sw_query = query.filter(db_models.StopWords.languageId == language_id[0])
-    if not include_system_defined:
-        sw_query = sw_query.filter(db_models.StopWords.confidence != 2)
-    if not include_user_defined:
-        sw_query = sw_query.filter(db_models.StopWords.confidence != 1)
-    if not include_auto_generated:
-        sw_query = sw_query.filter(db_models.StopWords.confidence >= 1)
-    if only_active:
-        sw_query = sw_query.filter(db_models.StopWords.active == only_active)
-    sw_query = sw_query.offset(skip).limit(limit).all()
-    result = []
-    for row in sw_query:
-        obj = {"stopword": row.stopWord, "confidence": row.confidence, "active": row.active}
-        result.append(obj)
-    return result
