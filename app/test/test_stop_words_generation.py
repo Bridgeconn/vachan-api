e0ea7f9280257c8fd31de8212218522d6269bc52
anjaly <anjalyv75@gmail.com>
2021-11-22 11:29:42 +0530

    included test code for stopword update

--- app/test/test_stop_words_generation.py
+++ app/test/test_stop_words_generation.py
@@ -6,6 +6,21 @@ from . import assert_not_available_content
 UNIT_URL = '/v2/lookup/stopwords'
 headers = {"contentType": "application/json", "accept": "application/json"}
 
+obj = {
+        "stopWord": "कहता",
+        "active": False,
+        "metaData": {
+        "type": "verb"
+     }
+    }
+wrong_obj =  {
+        "stopWord": "prayed",
+        "active": False,
+        "metaData": {
+        "type": "verb"
+     }
+    }
+
 def assert_positive_get_stopwords(item):
     '''Check for the properties in the normal return object'''
     assert "stopWord" in item
@@ -18,6 +33,11 @@ def assert_positive_get_stopwords(item):
 def test_get_default():
     '''positive test case, without optional params'''
     check_default_get(UNIT_URL+'/hi', assert_positive_get_stopwords)
+
+def assert_positive_update_stopwords(out):
+    '''Check the properties in the update response'''
+    assert "message" in out
+    assert "data" in out
 
 def test_get_stop_words():
     '''Positve tests for get stopwords API'''
@@ -51,3 +71,19 @@ def test_get_notavailable_code():
     ''' request a not available language_code'''
     response = client.get(UNIT_URL+"/abc")
     assert_not_available_content(response)
+
+def test_update_stopword():
+    '''Positve tests for update stopwords API'''
+    response = client.put(UNIT_URL+'/hi?',headers=headers, json=obj)
+    assert response.status_code == 200
+    assert_positive_update_stopwords(response.json())
+    for item in response.json()['data']:
+        assert_positive_get_stopwords(item)
+    if response.json()['message'] == "Stopword info updated successfully":
+        assert len(response.json()['data']) == 1
+
+    response = client.put(UNIT_URL+'/hi?',headers=headers, json=wrong_obj)
+    assert response.status_code == 200
+    assert_positive_update_stopwords(response.json())
+    if response.json()['message'] == "Couldn't update the provided data":
+        assert len(response.json()['data']) == 0
