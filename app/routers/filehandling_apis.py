46ae167233eb023b9991f3d7193b4ee8cc9f23c5
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2022-09-22 17:40:44 +0530

    fix linter complaints

--- app/routers/filehandling_apis.py
+++ app/routers/filehandling_apis.py
@@ -1,21 +1,16 @@
 '''API endpoints related to file format handling. Uses usfm-grammar'''
-
 from fastapi import APIRouter, Request, Path, Query, Depends, Body
 from sqlalchemy.orm import Session
 import usfm_grammar
-
-
 from dependencies import get_db, log
 from routers import content_apis
 from schema import schemas, schema_content
 from crud import files_crud
 from auth.authentication import get_auth_access_check_decorator ,\
     get_user_or_none
-from custom_exceptions import NotAvailableException, GenericException,\
-    UnprocessableException
+from custom_exceptions import NotAvailableException, GenericException
 
 router = APIRouter()
-
 #pylint: disable=too-many-arguments,unused-argument
 
 #################### USFM Grammar ####################
@@ -30,7 +25,7 @@ async def usfm_parse_source_bible(request: Request,
     book_code: schemas.BookCodePattern=Path(..., example="mat"),
     output_format: usfm_grammar.Format = Path(..., example="usx"),
     content_filter: usfm_grammar.Filter = Query(usfm_grammar.Filter.SCRIPTURE_PARAGRAPHS),
-    chapter: int=Query(None, example=1), 
+    chapter: int=Query(None, example=1),
     # verse: int=Query(None, example=1), last_verse: int=Query(None, example=15),
     active: bool=True,
     # skip: int=Query(0, ge=0), limit: int=Query(100, ge=0),
@@ -49,7 +44,7 @@ async def usfm_parse_source_bible(request: Request,
         user_details=user_details,
         db_=db_
         )
-    if "error" in src_response: 
+    if "error" in src_response:
         raise GenericException(src_response['error'])
     if len(src_response) == 0:
         raise NotAvailableException(f"Book, {book_code}, is not available in {source_name}")
@@ -57,16 +52,15 @@ async def usfm_parse_source_bible(request: Request,
     log.debug("Obtained usfm from source bible, %s", input_usfm[:50])
     return files_crud.parse_with_usfm_grammar(input_usfm, output_format, content_filter, chapter)
 
-
 @router.put('/v2/files/usfm/to/{output_format}',
     responses={422: {"model": schemas.ErrorResponse}, 500: {"model": schemas.ErrorResponse}},
     status_code=200, tags=['File Handling'])
 @get_auth_access_check_decorator
-async def parse_uploaded_usfm(request:Request, 
+async def parse_uploaded_usfm(request:Request,
     output_format: usfm_grammar.Format = Path(..., example="usx"),
     content_filter: usfm_grammar.Filter = Query(usfm_grammar.Filter.SCRIPTURE_PARAGRAPHS),
-    input_usfm: schema_content.UploadedUsfm = Body(...), 
-    chapter: int=Query(None, example=1), 
+    input_usfm: schema_content.UploadedUsfm = Body(...),
+    chapter: int=Query(None, example=1),
     # verse: int=Query(None, example=1), last_verse: int=Query(None, example=15),
     user_details=Depends(get_user_or_none)):
     '''Allows to upload a USFM file to be converted to another format. uses usfm-grammar'''
