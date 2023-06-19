6967304ad9fc75a59c65535ecc497cd24d55caeb
AthulyaMS <athulyamarysaji1996@gmail.com>
2023-06-19 12:26:59 +0530

    changes

--- app/test/test_agmt_translation.py
+++ app/test/test_agmt_translation.py
@@ -1069,10 +1069,16 @@ def test_agmt_translation_access_permissions():
         assert resp.status_code == 403
         assert resp.json()['error'] == "Permission Denied"
 
-        
+        data_str = json.dumps(post_obj_list)
         response = client.get(
             UNIT_URL + "/token-translations",
-         params=params,
+         params={
+                "project_id": str(project_id),
+                "token": all_tokens[0]['token'],
+                "sentence_id": "41001001",
+                "offset": ["0", "4"],
+                "data": data_str
+                },
              headers=headers_auth)
         assert response.status_code == 403
         assert response.json()['error'] == "Permission Denied"
