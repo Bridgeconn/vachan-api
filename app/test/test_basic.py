7add0100420d623934ddb09a16fdfaf5128ca78c
shimilgithub <shimil184@gmail.com>
2023-07-05 10:48:55 +0530

    Landing page - draft 1

--- app/test/test_basic.py
+++ app/test/test_basic.py
@@ -6,4 +6,4 @@ def test_default_api():
     '''test the default API endpoint and check if App is up'''
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "App is up and running"}
+    assert "<title>API Documentation </title>" in response.text
