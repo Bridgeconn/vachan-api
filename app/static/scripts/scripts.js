a038acae3a1a5e262bc22f00b7164fb8bb5e77af
joel-joseph-george <joelpadeesseril@gmail.com>
2023-10-04 11:48:24 +0530

    Added new figma ui designs

--- app/static/scripts/scripts.js
+++ app/static/scripts/scripts.js
@@ -21,7 +21,7 @@ TxtType.prototype.tick = function() {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
     var that = this;
-    var delta = 200 - Math.random() * 100;
+    var delta = 120 - Math.random() * 75;
 
     if (this.isDeleting) { delta /= 2; }
 
